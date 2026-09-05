d52e4469109a718d38fe9c903fdab6bc7d01e4a1
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-09-05 16:07:22 +0530

    feat: add load_data helper and endpoint to retrieve patient records from JSON file

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+# reading the data from json file
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
 
 @app.get("/")
 def hello():
@@ -9,3 +17,10 @@ def hello():
 @app.get("/about")
 def about():
     return {"message":"campusX is an education platform where you can learn ai"}
+
+@app.get("/view")
+def view_patients():
+    data = load_data()
+    return data
+
+#viewing a specific patient using patient id
