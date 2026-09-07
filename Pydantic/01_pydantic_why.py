79b539de8904d086c82e746ae700fcb926e0c00d
mohd ahtasham ansari <aahtasham102@gmail.com>
2026-09-07 17:31:53 +0530

    feat: add demonstration script for Pydantic data validation using Patient model

--- Pydantic/01_pydantic_why.py
+++ Pydantic/01_pydantic_why.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel
+from typing import List , Dict ,Optional
+
 
 # def insert_pateint(name:str ,age:int):
 
@@ -41,20 +43,32 @@ from pydantic import BaseModel
 class Patient(BaseModel):
     name:str
     age:int
+    weight:float
+    married:bool
+    allergies: Optional[List[str]]=None
+    contact_details: Dict[str,str]
 
 
 def insert_pateint_info(patient:Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
     print("pateint data inserted")
 
 def update_patient_info(pateint:Patient):
     print(pateint.name)
     print(pateint.age)
+    print(pateint.weight)
+    print(pateint.married)
+    print(pateint.allergies)
+    print(pateint.contact_details)
     print("pateint updated successfully")
 
 
-pateint_info = {"name":"ayush", "age":30}
+pateint_info = {"name":"ayush", "age":30,"weight":70.5,"married":True,"allergies":["peanuts","dust"],"contact_details":{"email":"[EMAIL_ADDRESS]","phone":"1234567890"}}
 
 patient1= Patient(**pateint_info)
 
@@ -62,34 +76,34 @@ insert_pateint_info(patient1)
 
 update_patient_info(patient1)
 
-""" lemme create an example of student info , data validation using pydantic  """
+# """ lemme create an example of student info , data validation using pydantic  """
 
-class Student(BaseModel):
-    name:str
-    class_std:int
-    age:int
-    roll_no:int
-    student_id:str
+# class Student(BaseModel):
+#     name:str
+#     class_std:int
+#     age:int
+#     roll_no:int
+#     student_id:str
 
-def insert_student_info(student:Student):
-    print(f"name - {student.name}")
-    print(f"class - {student.class_std}")
-    print(f"age - {student.age}")
-    print(f"roll no - {student.roll_no}")
-    print(f"student id - {student.student_id}")
-    print("Student data inserted")
+# def insert_student_info(student:Student):
+#     print(f"name - {student.name}")
+#     print(f"class - {student.class_std}")
+#     print(f"age - {student.age}")
+#     print(f"roll no - {student.roll_no}")
+#     print(f"student id - {student.student_id}")
+#     print("Student data inserted")
 
-def update_student_info(student:Student):
-    print(f"name - {student.name}")
-    print(f"class - {student.class_std}")
-    print(f"age - {student.age}")
-    print(f"roll no - {student.roll_no}")
-    print(f"student id - {student.student_id}")
-    print("Student data updated")
+# def update_student_info(student:Student):
+#     print(f"name - {student.name}")
+#     print(f"class - {student.class_std}")
+#     print(f"age - {student.age}")
+#     print(f"roll no - {student.roll_no}")
+#     print(f"student id - {student.student_id}")
+#     print("Student data updated")
 
-student_info={"name":"steve","class_std":12 , "age": 19,"roll_no":1, "student_id":"20120"}
+# student_info={"name":"steve","class_std":12 , "age": 19,"roll_no":1, "student_id":"20120"}
 
-student1=Student(**student_info)
+# student1=Student(**student_info)
 
 
-insert_student_info(student1)
+# insert_student_info(student1)
